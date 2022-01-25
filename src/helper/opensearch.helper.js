a827b929ee67990adb0efed40367c5e01bbef111
Julián Augusto Escudero <juescuder@gmail.com>
2022-01-25 21:55:48 +0100

    Added Map to OpenSearch hits.

--- src/helper/opensearch.helper.js
+++ src/helper/opensearch.helper.js
@@ -1,14 +1,6 @@
-const { OpenSearchClient, AcceptInboundConnectionCommand } = require("@aws-sdk/client-opensearch");
-const { HttpRequest} = require("@aws-sdk/protocol-http");
-const { defaultProvider } = require("@aws-sdk/credential-provider-node");
-const { SignatureV4 } = require("@aws-sdk/signature-v4");
-const { NodeHttpHandler } = require("@aws-sdk/node-http-handler");
-const { Sha256 } = require("@aws-crypto/sha256-browser");
-var { Client } = require('@opensearch-project/opensearch');
+const { Client } = require('@opensearch-project/opensearch');
 
-//https://www.npmjs.com/package/@opensearch-project/opensearch
-
-var OpenSearchHelper = {
+const OpenSearchHelper = {
 
     indexUser: function(userName, userId) {
 
@@ -54,7 +46,7 @@ var OpenSearchHelper = {
         var protocol = 'https';
         var port = 443;
         var auth = 'goblinMaster:LernerPresidente2022!'; // For testing only. Don't store credentials in code.
-        
+
         var index_name = 'username'
 
         var settings = {
@@ -62,7 +54,7 @@ var OpenSearchHelper = {
                 'index': {
                     'number_of_shards': 4,
                     'number_of_replicas': 3
-                }
+                    }
             }
         }
 
@@ -84,7 +76,7 @@ var OpenSearchHelper = {
         })
 
         return response.body.hits
-      }
+    }
 }
 
 module.exports = OpenSearchHelper
