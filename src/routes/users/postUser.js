e7e75ef99f99769222ca03c759dbcb649833885b
Deivid <david_recheni@hotmail.com>
2022-02-03 20:36:59 +0100

    Uppercase

--- src/routes/users/postUser.js
+++ src/routes/users/postUser.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const { Router } = require('express')
 const router = Router()
-const User = require('../../models/user')
+const User = require('../../models/User')
 const OpenSearchService = require('../../services/opensearch')
 
 /**
