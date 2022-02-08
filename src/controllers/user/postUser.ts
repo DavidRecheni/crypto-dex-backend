b67d18bec2339273fdd2dbf3ab8e1008070b09a2
Deivid <david_recheni@hotmail.com>
2022-02-08 21:59:56 +0100

    Updated model Fix body parser

--- src/controllers/user/postUser.ts
+++ src/controllers/user/postUser.ts
@@ -14,20 +14,22 @@ const router = Router();
 router.post('/user', (req, res) => {
   // #swagger.tags = ['User']
   // #swagger.description = 'Create a new user'
-
   const user = new User({
     _id: new mongoose.Types.ObjectId(),
-    name: req?.body?.name,
-    username: req?.body?.username,
-    wallet: req?.body?.wallet,
-    bio: req?.body?.bio,
-    avatar: req?.body?.avatar,
+    name: req.body?.name,
+    username: req.body?.username,
+    publicAddress: req.body?.publicAddress,
+    email: req.body?.email,
+    bio: req.body?.bio,
+    avatar: req.body?.avatar,
   });
 
   user.save().then((newUser: UserType) => {
     indexUser(newUser.username, newUser._id.toString());
-    res.status(200).json(responseBuilder({ data: newUser }));
-  }).catch(() => {
+    // TODO: Reeplace with token
+    res.status(200).json(responseBuilder({ data: {...newUser, token: 'asdasd123123'} }));
+  }).catch((e: any) => {
+    console.log('error creating user', e);
     res.status(200).json(responseBuilder({ error: ERROR_CODES.User.UnableToCreate }));
   });
 });
