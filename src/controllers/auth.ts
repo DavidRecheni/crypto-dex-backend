069b264aeb4477fe86ce6d07b49d5b79b179dbb7
Julián Augusto Escudero <juescuder@gmail.com>
2022-02-10 18:28:49 +0100

    Added Auth Controller

---
+++ src/controllers/auth.ts
@@ -0,0 +1,33 @@
+import express, { Router } from 'express';
+import ERROR_CODES from '../constant';
+import responseBuilder from '../utils/responseBuilder';
+import mongoose from 'mongoose';
+import User, { UserType } from '../models/User';
+
+const router = Router();
+
+/**
+ * Get nonce from public wallet address
+ */
+ router.get('/auth/:publicAddress', async (req:express.Request, res:express.Response) => {
+  // #swagger.tags = ['Auth']
+  // #swagger.description = '* Get nonce from public wallet address'
+
+  console.log(req.params.publicAddress);
+
+  const publicAddress = req.params.publicAddress;
+  let result = {};
+
+  try {
+    let data = await User.findOne({ wallet: publicAddress }).select({"_id": 0, "nonce": 1, "publicAddress": 1 }).exec();
+    result = responseBuilder(data);
+  } catch (error) {
+    console.log(error)
+    result = responseBuilder({ error: ERROR_CODES.Wallet.NotFound });
+  }
+
+  res.status(200).json(result);
+
+});
+
+export default router;
