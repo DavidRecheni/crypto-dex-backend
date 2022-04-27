07038de35e3e34d42bb474aa0d587aa92c37e2c3
Deivid <david_recheni@hotmail.com>
2022-04-27 10:03:18 +0200

    added default avatar parts

--- src/constants/defaultAvatarData.ts
+++ src/constants/defaultAvatarData.ts
@@ -3,6 +3,11 @@ import defaultAvatarImage from '../assets/images/defaultAvatarImage';
 const defaultAvatarData = (userId: string) => ({
   userId,
   url: defaultAvatarImage,
+  parts: [
+    '6267fa8d987ec3c860b95c7b',
+    '6268e7ac09c472b6aae4f127',
+    '6268e8cd09c472b6aae4f12b',
+  ],
 });
 
 export default defaultAvatarData;
